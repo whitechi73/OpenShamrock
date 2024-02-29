9546e90bec62dfb0d4e0d143150863110ac01a9b
huan_kong <2564076459@qq.com>
2024-03-01 00:23:46 +0800

    修正报错无法通过 Websocket 返回

--- xposed/src/main/java/moe/fuqiuluo/shamrock/remote/action/handlers/SendGroupMessage.kt
+++ xposed/src/main/java/moe/fuqiuluo/shamrock/remote/action/handlers/SendGroupMessage.kt
@@ -1,6 +1,7 @@
 package moe.fuqiuluo.shamrock.remote.action.handlers
 
 import com.tencent.qqnt.kernel.nativeinterface.MsgConstant
+import moe.fuqiuluo.shamrock.helper.ParamsException
 import moe.fuqiuluo.shamrock.remote.action.ActionSession
 import moe.fuqiuluo.shamrock.remote.action.IActionHandler
 import moe.fuqiuluo.shamrock.tools.jsonArray
@@ -9,19 +10,25 @@ import moe.fuqiuluo.symbols.OneBotHandler
 @OneBotHandler("send_group_message", ["send_group_msg"])
 internal object SendGroupMessage: IActionHandler() {
     override suspend fun internalHandle(session: ActionSession): String {
-        val groupId = session.getLong("group_id")
-        val retryCnt = session.getIntOrNull("retry_cnt")
-        val recallDuration = session.getLongOrNull("recall_duration")
-        return if (session.isString("message")) {
-            val autoEscape = session.getBooleanOrDefault("auto_escape", false)
-            val message = session.getString("message")
-            SendMessage(MsgConstant.KCHATTYPEGROUP, groupId.toString(), message, autoEscape, echo = session.echo, retryCnt = retryCnt ?: 5, recallDuration = recallDuration)
-        } else if (session.isObject("message")) {
-            val message = session.getObject("message")
-            SendMessage(MsgConstant.KCHATTYPEGROUP, groupId.toString(), listOf( message ).jsonArray, session.echo, retryCnt = retryCnt ?: 5, recallDuration = recallDuration)
-        } else {
-            val message = session.getArray("message")
-            SendMessage(MsgConstant.KCHATTYPEGROUP, groupId.toString(), message, session.echo, retryCnt = retryCnt ?: 5, recallDuration = recallDuration)
+        try{
+            val groupId = session.getLong("group_id")
+            val retryCnt = session.getIntOrNull("retry_cnt")
+            val recallDuration = session.getLongOrNull("recall_duration")
+            return if (session.isString("message")) {
+                val autoEscape = session.getBooleanOrDefault("auto_escape", false)
+                val message = session.getString("message")
+                SendMessage(MsgConstant.KCHATTYPEGROUP, groupId.toString(), message, autoEscape, echo = session.echo, retryCnt = retryCnt ?: 5, recallDuration = recallDuration)
+            } else if (session.isObject("message")) {
+                val message = session.getObject("message")
+                SendMessage(MsgConstant.KCHATTYPEGROUP, groupId.toString(), listOf( message ).jsonArray, session.echo, retryCnt = retryCnt ?: 5, recallDuration = recallDuration)
+            } else {
+                val message = session.getArray("message")
+                SendMessage(MsgConstant.KCHATTYPEGROUP, groupId.toString(), message, session.echo, retryCnt = retryCnt ?: 5, recallDuration = recallDuration)
+            }
+        } catch (e: ParamsException) {
+            return noParam(e.message!!, session.echo)
+        } catch (e: Throwable) {
+            return logic(e.message ?: e.toString(), session.echo)
         }
     }
 
--- xposed/src/main/java/moe/fuqiuluo/shamrock/remote/action/handlers/SendGroupSign.kt
+++ xposed/src/main/java/moe/fuqiuluo/shamrock/remote/action/handlers/SendGroupSign.kt
@@ -1,7 +1,10 @@
 package moe.fuqiuluo.shamrock.remote.action.handlers
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonElement
 import moe.fuqiuluo.qqinterface.servlet.GroupSvc
+import moe.fuqiuluo.qqinterface.servlet.structures.GuildInfo
 import moe.fuqiuluo.shamrock.remote.action.ActionSession
 import moe.fuqiuluo.shamrock.remote.action.IActionHandler
 import moe.fuqiuluo.shamrock.tools.EmptyJsonString
@@ -17,11 +20,16 @@ internal object SendGroupSign: IActionHandler() {
     suspend operator fun invoke(groupId: Long, echo: JsonElement = EmptyJsonString): String {
         val ret = GroupSvc.groupSign(groupId)
         return if (ret.isSuccess) {
-            ok(ret.getOrNull() ?: "", echo)
+            ok(Message(message = ret.getOrNull() ?: ""), echo, "成功")
         } else {
             logic(ret.exceptionOrNull()?.message ?: "", echo)
         }
     }
 
     override val requiredParams: Array<String> = arrayOf("group_id")
+
+    @Serializable
+    data class Message(
+        @SerialName("message") var message: String = ""
+    )
 }
