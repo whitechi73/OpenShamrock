a3dc0d06b26cec3e36333755fdc805ad926a6371
tuuz <silverhawk@foxmail.com>
2024-03-18 03:17:36 +0800

    新增get_file方法，主要解决使用反向websocket的时候获取文件麻烦的问题，目前仅支持base64的type返回，未来将支持更多模式，测试后将发布至文档

--- xposed/src/main/java/moe/fuqiuluo/shamrock/remote/service/data/ResourceData.kt
+++ xposed/src/main/java/moe/fuqiuluo/shamrock/remote/service/data/ResourceData.kt
@@ -3,13 +3,11 @@ package moe.fuqiuluo.shamrock.remote.service.data
 import kotlinx.serialization.Serializable
 import java.util.Base64
 
-@Serializable
-internal data class OutResource(
-    val file: String,
-    val url: String
+@Serializable internal data class OutResource(
+    val file: String, val url: String
 )
 
-internal data class OutResourceByBase64(
+@Serializable internal data class OutResourceByBase64(
     val file: String,
     val base64String: String,
 )
